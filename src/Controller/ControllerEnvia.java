3563729d9d05c2bfcddd04fae0cc53d3a3f402d3
Thatianne Cristina <thatianne.cristina@hotmail.com>
2016-08-31 19:51:56 -0300

    Tratar recebimento (envio concluido)

--- src/Controller/ControllerEnvia.java
+++ src/Controller/ControllerEnvia.java
@@ -16,6 +16,7 @@ import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -30,6 +31,7 @@ public class ControllerEnvia {
     private final Via destino;
     private int preferencia;
     private String hora;
+    private String horaFinal;
     private String[] msgCarros;
     
     public ControllerEnvia(Via origem, Via destino, int pref, String[] msgCarros) {
@@ -43,7 +45,7 @@ public class ControllerEnvia {
         // hora de inicio
         hora = geraHoraInicial();
         //hora final, que termina  ação requisitada
-        
+        horaFinal = geraHoraFinal(origem, destino, hora);
         
         while (true) {
             byte[] b = null;
@@ -90,8 +92,34 @@ public class ControllerEnvia {
         return dataFormatada;
     }
     
-    private String geraHoraFinal(){
-        String hora = null;
+    private String geraHoraFinal(Via origem, Via destino, String horaInicial){
+        String hora = null, minString;
+        int segundos = 0, min;
+        String direcao = rota(origem, destino);
+        if(direcao.equals("direto")){
+            //Gasta 18,5 segundos
+            segundos = 19;
+        }else if(direcao.equals("direita") || direcao.equals("esquerda")){
+            //Gasta 36 segundos (21.6 + 14.4)
+            segundos = 36;            
+        }
+        //Adicionar esses segundos à hora inicial
+        
+        String segString = horaInicial.substring(6, 8);
+        int segInt = Integer.parseInt(segString);        
+        segundos = segundos + segInt;
+        if(segundos >= 60){//adicionar 1 minuto
+            minString = horaInicial.substring(3, 5);
+            min = Integer.parseInt(minString);
+            min = min + 1;
+            segundos = segundos % 60;
+            hora = horaInicial.substring(0, 3)+min+":"+ segundos;
+            System.out.println(hora);
+        }else{
+            hora = horaInicial.substring(0,6)+segundos;
+            System.out.println(hora);
+        }                  
+        
         return hora;
     }
     
@@ -105,9 +133,75 @@ public class ControllerEnvia {
     
     private float calTempo(float vFinal, float vInicial, float aceleracao){
         float tempo;
-        tempo = (vFinal - vInicial)/aceleracao;
-        return tempo;//tempo em horas
+        tempo = (vFinal - vInicial)/aceleracao;//tempo em horas
+        tempo = tempo * 360;//tempo em segundos
+        return tempo;
     }
     
-
+    private String rota(Via origem, Via destino){
+        int i = 0;
+        String concatenado = origem.getVia()+ " "+ destino.getVia();
+        String direcao;
+        
+        switch(concatenado){
+            //direto
+            case "Norte Sul":{
+                direcao = "direto";
+                break;
+            }
+            
+            case "Sul Norte":{
+                direcao = "direto";
+                break;
+            }
+            case "Leste Oeste":{
+                direcao = "direto";
+                break;
+            }
+            case "Oeste Leste":{
+                direcao = "direto";
+                break;
+            }
+            //direita
+            case "Norte Oeste":{
+                direcao = "direita";
+                break;                
+            }
+            case "Leste Norte":{
+                direcao = "direita";
+                break;
+            }
+            case "Sul Leste":{
+                direcao = "direita";
+                break;
+            }
+            case "Oeste Sul":{
+                direcao = "direita";
+                break;
+            }
+            //esquerda
+            case "Norte Leste":{
+                direcao = "esquerda";
+                break;
+            }
+            case "Leste Sul":{
+                direcao = "esquerda";
+                break;
+            }
+            case "Sul Oeste":{
+                direcao = "esquerda";
+                break;
+            }
+            case "Oeste Norte":{
+                direcao = "esquerda";
+                break;
+            }
+            //origem == destino
+            default:{
+                direcao = "erro";
+                break;
+            }      
+        }
+        return direcao;
+    }
 }
